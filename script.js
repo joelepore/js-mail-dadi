9396c6e3cff799f7a7ef8dc5770e68357e749205
joelepore <giuseppejoelepore@gmail.com>
2024-11-18 15:02:07 +0100

    build: add base project files

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. Non è necessario provvedere alla validazione delle email
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+/*
+  1. Dichiaro e inizializzo un array di email
+  2. Chiedo in input un'email
+  3. Verifico che l'email sia registrata
+    - Se e' registrata, vado avanti
+    - Altrimenti la richiedo
+*/
